8e53792c24b9b38e89bf572a265a67debe91b358
Kevin Huang <yellow.khaos@gmail.com>
2018-10-03 21:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function updateDriverWithKeyAndValue (driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  
+  driver[key] = value
+  return driver
 }
